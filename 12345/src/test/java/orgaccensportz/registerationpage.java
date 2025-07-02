3b5af261f329ea6dcaf92bdacef73cbc304b3268
Loki0312 <logesh.ravichandran@cdc2-l-42864v1.dir.svc.accenture.com>
2025-07-02 13:09:09 +0530

    update

--- 12345/src/test/java/orgaccensportz/registerationpage.java
+++ 12345/src/test/java/orgaccensportz/registerationpage.java
@@ -36,7 +36,7 @@ public class registerationpage {
 	  Actions a2 =new Actions(driver);
 	  a2.moveToElement(w2).doubleClick().build().perform();
 	  driver.findElement(By.xpath("/html/body/app-root/app-main/app-form/div/form/button")).click();	  
-	  
+	  System.out.println("hello");
   
   }
   @BeforeMethod
